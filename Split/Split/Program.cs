d35e1917b433bc3356df5a1883cf8b2c105407b9
AvanMyers <smail1341@gmail.com>
2022-12-16 17:00:20 +0300

    done

---
+++ Split/Split/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringToWrite = "";
+
+            Console.WriteLine("Введите предложение для переноса по строкам");
+            stringToWrite = Console.ReadLine();
+            string[] words = stringToWrite.Split(' ');
+
+            foreach (string word in words)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
